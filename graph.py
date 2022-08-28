8b0cd70fe9902fa366d12facbc7cddc4a191c2d7
Anubhav pratap singh <91564916+anubhav6488@users.noreply.github.com>
2022-08-28 13:23:37 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4]
+y=[100,250,400,900]
+plt.plot(x,y)
+plt.show()
---
+++ random selector.py
@@ -0,0 +1,8 @@
+import random
+person=[]
+for x in range(0,8):
+
+ people=input("type the name of the person ")
+ person.append(people)
+index=random.randint(0,7)
+print(person[index])
